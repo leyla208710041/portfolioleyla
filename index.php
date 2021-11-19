75faf185784b074fec79f1cab69e4d0f0a41deda
leyla208710041 <93367398+leyla208710041@users.noreply.github.com>
2021-11-19 14:54:43 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Abdykalykova Leyla</title>
+</head>
+
+<style>
+h1 {
+font-family: verdana;
+}
+
+h3 {
+color: rgb(255, 0, 0);
+font-family: serif;
+}
+
+p {
+font-family: Helvetica;
+}
+
+.button {
+border: none;
+color: rgb(255, 255, 255);
+padding: 32px 64px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+margin: 8px 4px;
+transition-duration: 0.9s;
+cursor: pointer;
+}
+
+.button1 {
+background-color: rgba(248, 240, 248, 0);
+color: rgba(0, 0, 0, 0);
+border: 2px solid rgba(0, 0, 0, 0);
+}
+/* Github */
+
+.button1:hover {
+background-color: black;
+color: white;
+}
+
+.button2 {
+background-color: white;
+color: black;
+border: 2px solid rgb(255, 20, 20);
+}
+/* Instagram */
+
+.button2:hover {
+background-color: rgb(255, 20, 20);
+color: white;
+}
+.button3 {
+background-color: white;
+color: rgb(255, 0, 0);
+border: 2px solid rgb(255, 30, 30);
+}
+/* Telegram */
+
+.button3:hover {
+background-color: rgb(245, 245, 245);
+color: black;
+}
+
+</style>
+<center>
+
+<body style="background-color:rgb(212, 209, 21);">
+<br>
+<br>
+<br>
+<h1>Leyla Abdykalykova <br>student of Ala-Too university</h1>
+<h3>contact information :
+    phone number 0 777 777 777
+    address ankara 1/8
+</h3>
+<p> I'm creating my new style.</p>
+<img src="иуу.jpg" alt='
+' style=''>
+<form action='' target='_blank'>
+<button class="button button1">Github</button>
+</form>
+<form action='https://www.instagram.com/madcoffeezombie/' target='_blank'>
+<button clas
+<button class="button button1">instagramm</button>
